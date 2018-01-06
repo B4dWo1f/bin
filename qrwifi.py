7890524036aadca13028b51b154e7ad97637fbd6
BadWolf Raven <noel.garcia@openmailbox.org>
2018-01-06 21:51:15 +0100

    Code to create the QR code for every stored wifi password

---
+++ qrwifi.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+""" OJO!!! This script requires sudo """
+
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+try: import qrcode
+except ImportError:
+   msg = 'python-qrcode not found. You can install it like this:\n'
+   msg += 'git clone https://github.com/lincolnloop/python-qrcode.git\n'
+   msg += 'cd python-qrcode\n'
+   msg += 'sudo python3 setup.py install'
+   sys.exit(msg)
+import qrcode.image.svg
+USER = os.getenv('USER')
+
+
+## Setup the output folder
+try: out = sys.argv[1]
+except IndexError: out = './'
+if out[-1] != '/': out += '/'
+os.system('mkdir -p %s'%(out))
+if not os.path.isdir(out):
+   print('Destination folder does not exist and could not be created')
+   exit()
+
+## Read the network files
+fol = '/etc/NetworkManager/system-connections/'
+onlyfiles = [ f for f in listdir(fol) if isfile(join(fol,f)) ]
+print('WARNING. I need sudo permission for reading the files in %s'%(fol))
+for f in onlyfiles:
+   f = os.popen('sudo cat "%s"'%(fol+f)).read()
+   for l in f.splitlines():
+      if 'ssid=' in l: ssid = l.split('=')[1]
+      elif 'psk=' in l: pwd = l.split('=')[1]
+   try: ssid,pwd
+   except NameError:
+      print('Error')
+      print(f)
+      continue
+   img = qrcode.make(pwd)
+   img.save('/tmp/test.png')
+   img = plt.imread('/tmp/test.png')
+   fig, ax = plt.subplots()
+   ax.imshow(img,'Greys_r')
+   ax.set_title(ssid,fontsize=30)
+   ax.set_xticks([])
+   ax.set_yticks([])
+   #ax.set_xlabel(pwd)
+   ax.set(aspect=1)
+   fig.tight_layout()
+   fig.savefig(out+ssid+'.png')
+   print(out+ssid+'.png')
+   #plt.show()
+   del ssid
+   del pwd
