6642d2ed7c193aa4041957d05331d689f016246f
no3l <noel.garcia@openmailbox.org>
2019-01-04 18:03:40 +0100

    added latex to epub script

---
+++ latex2epub.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# Will only work for very basic tex files
+#
+
+latexml --dest=$1.xml $1.tex
+latexmlpost -dest=$1.html $1.xml
+ebook-convert $1.html $1.epub --language en --no-default-epub-cover
