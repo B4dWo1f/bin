cc22cc3239e8378919894faec6ab85e28aa901b0
no3l <noel.garcia@openmailbox.org>
2019-02-23 12:34:25 +0100

    modification in mouse_heat

--- mouse_heat.py
+++ mouse_heat.py
@@ -12,6 +12,10 @@ from time import sleep
 T = 10 # minutes
 t = 0.5   # sleep time
 
+screen = os.popen('xrandr | grep -w connected').read().strip()
+px = screen.split('primary')[1].split()[0].split('+')[0]
+xs,ys = map(int,px.split('x'))
+
 N = T*60 /t
 
 X,Y = [],[]
@@ -26,4 +30,6 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 ax.scatter(X,Y,edgecolor='none',alpha=0.5)
 ax.set_title('Position of mouse in screen')
+ax.set_xlim([0,xs])
+ax.set_ylim([ys,0])
 plt.show()
