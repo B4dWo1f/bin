f401c89e3a8ad84d9c1846260c8d76cf82e36e2d
B4dWo1f <B4dWo1f@protonmail.com>
2019-08-28 16:59:26 +0200

    create log file before sudo to fix permissions

--- mylogkeys.sh
+++ mylogkeys.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 RUNNING=`ps -e | grep -w logkeys`
+FILE=$HOME/.log.txt
 
+touch $FILE
 if [[ -z $RUNNING ]] ; then
    echo "STARTING logkeys"
-   sudo logkeys -m $HOME/.logkeys/maps/en_US.map -o $HOME/log.txt -s
+   sudo logkeys -m $HOME/.logkeys/maps/en_US.map -o $FILE -s
 else
    echo "STOPPING logkeys"
    sudo logkeys -k
