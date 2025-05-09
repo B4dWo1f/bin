ea0202132fc7e9406a9ef3a9e58074b9bb80cd8f
Noel <B4dWo1f@protonmail.com>
2025-05-09 15:02:42 +0200

    strip input in timelapse

--- timelapse.py
+++ timelapse.py
@@ -12,7 +12,7 @@ except:
    print('timelapse.py video_name fps file.txt')
    exit()
 
-ext = open(fname,'r').readlines()[0].split('.')[-1]
+ext = open(fname,'r').readlines()[0].split('.')[-1].strip()
 
 com = f'mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4 -o {vid_name} -mf type={ext}:fps={fps} mf://@{fname}'
 print(com)
